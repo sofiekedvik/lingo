ad4da5712b2015679cd35c162a98d1f4305e19a2
Sofie kedvik <sofie.kedvik@gmail.com>
2015-05-27 11:41:43 +0200

    rutor tillagda och skriver ut bokstäver

--- js/game.js
+++ js/game.js
@@ -6,6 +6,11 @@ console.log(valtOrd);
 var button = document.getElementsByTagName('button')[0]; //måste sätta index för tagName tar ut arrayer.
 button.addEventListener("click", function(){
         var anvandarOrd = document.getElementsByTagName("input")[0].value; //hämtar och sparar ut värdet direkt.
+        var rutor = document.getElementsByTagName('td');   
+        for(var i = 0; i < 5; i++){
+            rutor[i].innerHTML = anvandarOrd[i];
+        }
+    
         if (valtOrd === anvandarOrd){
             alert('Grattis!');
         }
